896191df2ffa8c64f3f203109d47cd09f719e693
Mate Sichinava <sich.mate@gmail.com>
2021-06-26 16:15:44 +0400

    removed redux extension

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ import { CssBaseline } from "@material-ui/core";
 
 import App from "./App";
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunck),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducers, compose(applyMiddleware(thunck)));
 
 ReactDOM.render(
   <Provider store={store}>
