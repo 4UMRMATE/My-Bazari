3631215d732dd90ea5d47f8ff5a76025f195e430
Mate Sichinava <sich.mate@gmail.com>
2021-06-26 16:56:04 +0400

    updated products grid

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -19,21 +19,23 @@ const Products = () => {
 
   return (
     <div className="Products">
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
-          {products.map((product) => (
-            <Grid item key={product._id} xs={12} sm={6} md={4}>
-              <Link
-                to={`/product/${product._id}`}
-                style={{ textDecoration: "none" }}
-              >
-                <Product data={product} />
-              </Link>
-            </Grid>
-          ))}
-        </Grid>
-        {/* <Paginator /> */}
-      </Container>
+      {products && (
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {products.map((product) => (
+              <Grid item key={product._id} xs={12} sm={6} md={4}>
+                <Link
+                  to={`/product/${product._id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Product data={product} />
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
+          {/* <Paginator /> */}
+        </Container>
+      )}
     </div>
   );
 };
