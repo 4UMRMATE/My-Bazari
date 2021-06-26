9aa8cec8a8c7b1f8597d95bc6db9570d6eb4d8e4
Mate Sichinava <sich.mate@gmail.com>
2021-06-26 17:20:35 +0400

    removed console.log

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -21,7 +21,6 @@ const Products = () => {
     <div className="Products">
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
-          {console.log(products, typeof products)}
           {products.map((product) => (
             <Grid item key={product._id} xs={12} sm={6} md={4}>
               <Link
